378a202d375b1e425de9a653aca3076c0a05d1a3
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-19 13:55:51 +0000

    able to render items when toggle is clicked

--- src/ShoppingCart/components/ShoppingItems/index.js
+++ src/ShoppingCart/components/ShoppingItems/index.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useShoppingContextValues } from "../../hooks";
 import { ShoppingCard } from "../ShoppingCard";
+import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 export const ShoppingItems = () => {
   const {
     state: { shoppingItems },
   } = useShoppingContextValues();
 
+  const [itemsToggle, useItemToggle] = useState(true);
+
+  const ShoppingItemToggle = () => {
+    useItemToggle(!itemsToggle);
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        padding: "30px",
-      }}
-    >
-      {shoppingItems.map((eachItem, index) => {
-        return <ShoppingCard {...eachItem} key={index} />;
-      })}
-    </Box>
+    <Container>
+      <Button fullWidth onClick={ShoppingItemToggle}>
+        Shopping Items
+      </Button>
+      {itemsToggle && (
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {shoppingItems.map((eachItem, index) => {
+            return <ShoppingCard {...eachItem} key={index} id={eachItem.id} />;
+          })}
+        </Box>
+      )}
+    </Container>
   );
 };
